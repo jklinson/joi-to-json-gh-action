9eb3e0ddc2d0c0c9d2f73bfb38b8b57ecb10ea8f
Linson Alfred <jklinson@gmail.com>
2022-02-14 17:06:16 +0700

    relative path

--- src/utils/schema-to-json.js
+++ src/utils/schema-to-json.js
@@ -1,10 +1,11 @@
 const parse = require('joi-to-json');
 const fs = require('fs');
+const path = require('path');
 const UserSchema = require('../schema/user');
 
 const dataToWrite = JSON.stringify(parse(UserSchema), null, 2);
 
-fs.writeFile('./resources/user.json', dataToWrite, 'utf8', (err) => {
+fs.writeFile(path.resolve(__dirname, '../../resources/user.json'), dataToWrite, 'utf8', (err) => {
 
     if (err) {
         console.log(`Error writing file: ${err}`);
