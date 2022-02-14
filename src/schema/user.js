7166d338f4cbd45498125895558b537ead0c1801
Linson Alfred <jklinson@gmail.com>
2022-02-14 18:34:40 +0700

    schema update

--- src/schema/user.js
+++ src/schema/user.js
@@ -4,7 +4,8 @@ const user = joi.object({
   email: joi.string().email().required(),
   phone_number: joi.string().optional(),
   first_name: joi.string().required(),
-  last_name: joi.string().required()
+  last_name: joi.string().required(),
+  dob: joi.date().iso().optional()
 });
 
 module.exports = user;
